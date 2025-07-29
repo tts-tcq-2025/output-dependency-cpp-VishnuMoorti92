6dc2bd0287511af43b7157bb4c7fdd2e655805ad
VishnuMoorti92 <bhatvishnuy@gmail.com>
2025-07-29 09:44:14 +0530

    Update main-test.cpp
    Test code correction

--- main-test.cpp
+++ main-test.cpp
@@ -1,15 +1,11 @@
 #include "gtest/gtest.h"
 
 void testTshirtSize();
-void testTshirtSize1();
-void testTshirtSize2();
 void testPrintColorMap();
 void testWeatherReport();
 
 TEST(TShirt, Size) {
     testTshirtSize();
-    testTshirtSize1();
-    testTshirtSize2();
 }
 
 TEST(ColorMap, Print) {
